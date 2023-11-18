3d1fa1305dc6ccdd28ac8bcfdfa197176063239a
h662 <h662hong@gmail.com>
2023-11-18 15:52:53 +0900

    Detail Page

---
+++ frontend/src/pages/detail.tsx
@@ -0,0 +1,44 @@
+import { FC, useEffect } from "react";
+import Header from "../components/Header";
+import { useMe } from "../hooks";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
+import ko from "date-fns/locale/ko";
+import { useSearchParams } from "react-router-dom";
+
+const Detail: FC = () => {
+  const { account, getMe } = useMe();
+
+  const [searchParams] = useSearchParams();
+
+  const title = searchParams.get("title");
+  const userAccount = searchParams.get("user-account");
+  const createdAt = searchParams.get("created-at");
+  const content = searchParams.get("content");
+
+  useEffect(() => {
+    getMe();
+  }, []);
+
+  return (
+    <>
+      <Header account={account} />
+      <main className="max-w-screen-md mx-auto py-24">
+        <div className="border-b-2">
+          <h1 className="text-center font-bold py-8 text-2xl">{title}</h1>
+          <div className="text-right pb-2 text-sm px-20">
+            <span>{userAccount}, </span>
+            <span>
+              {formatDistanceToNow(new Date(createdAt!), {
+                locale: ko,
+                addSuffix: true,
+              })}
+            </span>
+          </div>
+        </div>
+        <div className="px-20 pt-12 min-h-[360px]">{content}</div>
+      </main>
+    </>
+  );
+};
+
+export default Detail;
