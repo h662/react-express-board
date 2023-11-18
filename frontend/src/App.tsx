29bd6a672b082a5a35474a1b1c35eec54cf96f22
h662 <h662hong@gmail.com>
2023-11-18 13:04:13 +0900

    Sign In, Sign Up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,16 @@ import { FC } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Main from "./pages/main";
+import SignIn from "./pages/sign-in";
+import SignUp from "./pages/sign-up";
 
 const App: FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/log-in" element={<Main />} />
-        <Route path="/sign-up" element={<Main />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
         <Route path="/create" element={<Main />} />
         <Route path="/:postId" element={<Main />} />
       </Routes>
